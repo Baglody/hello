aac2b108af166b41f57e49232bfb430b4f961608
Baglody <rao.adithya@gmail.com>
2013-09-16 10:02:37 -0700

    this

--- 1.py
+++ 1.py
@@ -1,13 +1,20 @@
 import re
-filename="iv"
-f=open(filename+".c","r")
-a=f.read()
-get_all_ver_nos=re.findall("v.\..\...",a)
-print get_all_ver_nos
-max_version_no=get_all_ver_nos[0];
-for index in range(1,len(get_all_ver_nos)):
-	if get_all_ver_nos[index]>max_version_no:
-		max_version_no=get_all_ver_nos[index]
+import os
 
-print max_version_no
+def print_ver_no(filename):
+	#filename="iv"
+	with open("./"+filename+"/"+filename+".c","r") as f:
+		a=f.read()
+		f.closed
+	get_all_ver_nos=re.findall("v.\..\...",a)
+	print get_all_ver_nos
+	max_version_no=get_all_ver_nos[0];
+	for index in range(1,len(get_all_ver_nos)):
+		if get_all_ver_nos[index]>max_version_no:
+			max_version_no=get_all_ver_nos[index]
+
+	print max_version_no
 	
+	
+
+print_ver_no("iv")
